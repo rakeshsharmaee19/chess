8befc6b026be4601becb7d8ef528f72489e4bd1d
Rakesh Sharma <rakesh.sharma.ee19@gmail.com>
2023-12-04 04:04:39 +0530

    Create tournament.py

---
+++ models/tournament.py
@@ -0,0 +1,34 @@
+# -*- coding: utf-8 -*-
+import json
+import io
+
+class Tournament_Model:
+    """
+        Model to save Tournament data to json file and fetch bata from json
+    """
+    def save_data(self, data):
+        """
+            Method to save tournament data to tournament.json file
+        """
+        try:
+            to_unicode = unicode
+        except NameError:
+            to_unicode = str
+        with io.open('json_data/tournament.json', 'w', encoding='utf8') as tournament:
+
+            str_ = json.dumps(data,
+                              indent=4, sort_keys=True,
+                              separators=(',', ': '), ensure_ascii=False)
+            tournament.write(to_unicode(str_))
+
+    def get_data(self):
+        """
+            Method to Fetch JSON data of Tournament
+        """
+        try:
+            with open('json_data/tournament.json', 'r+') as data_file:
+                data_loaded = json.load(data_file)
+                return data_loaded
+        except (json.JSONDecodeError, FileNotFoundError):
+            return None
+
