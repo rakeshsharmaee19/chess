21c508fbb59bb83017b49b9f416d1e4c9ba0dace
Rakesh Sharma <rakesh.sharma.ee19@gmail.com>
2023-12-04 04:04:11 +0530

    Create score.py

---
+++ models/score.py
@@ -0,0 +1,31 @@
+# -*- coding: utf-8 -*-
+import json
+import io
+
+
+class Score_Model:
+    """
+        Model to save player score data to json file and fetch data from json
+    """
+
+    def save_data(self, data):
+        try:
+            to_unicode = unicode
+        except NameError:
+            to_unicode = str
+        with io.open('json_data/score.json', 'w', encoding='utf8') as player:
+            str_ = json.dumps(data,
+                              indent=4,
+                              separators=(',', ': '), ensure_ascii=False)
+            player.write(to_unicode(str_))
+
+    def get_data(self):
+        """
+            Method to Fetch JSON data of player
+        """
+        try:
+            with open('json_data/score.json', 'r+') as data_file:
+                data_loaded = json.load(data_file)
+                return data_loaded
+        except (json.JSONDecodeError, FileNotFoundError):
+            return None
